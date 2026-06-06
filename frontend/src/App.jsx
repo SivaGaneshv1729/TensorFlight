3888523a8309b0ec7fcd287f38c34321fcf3119f
sivaganeshv1729 <sivaganeshv1729@gmail.com>
2026-06-06 22:59:19 +0530

    feat(frontend): setup main application entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import HUD from './components/HUD'
+import VideoContainer from './components/VideoContainer'
+import Sidebar from './components/Sidebar'
+import Scene from './canvas/Scene'
+
+import useWebSocket from './useWebSocket'
+
+import useTelemetryStore from './store/useTelemetryStore'
+
+function App() {
+  useWebSocket()
+  const telemetry = useTelemetryStore((state) => state.telemetry)
+
+  return (
+    <div className="relative w-screen h-screen bg-black text-white overflow-hidden font-sans">
+      {/* DEBUG READOUT - REMOVE LATER */}
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 z-[100] bg-red-600/80 text-[10px] p-1 font-mono">
+        LAT: {telemetry.drone_state.gps.latitude.toFixed(4)} | ALT: {telemetry.drone_state.gps.altitude_relative_m.toFixed(1)}
+      </div>
+      {/* 3D Holographic Overlay */}
+      <div className="absolute inset-0 z-10 pointer-events-none">
+        <Scene />
+      </div>
+
+      {/* Background Video Stream */}
+      <div className="absolute inset-0 z-0">
+        <VideoContainer />
+      </div>
+
+      {/* Augmented Reality HUD Elements */}
+      <div className="absolute inset-0 z-20 pointer-events-none">
+        <HUD />
+      </div>
+
+      {/* Control Sidebar */}
+      <div className="absolute top-0 right-0 h-full z-30">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
